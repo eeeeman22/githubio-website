5b15c99ba867efc5c73603a99e2662ec5e14ab7b
eeeeman <eeeeman@icloud.com>
2020-01-28 22:17:33 -0500

    Updates

---
+++ precache-manifest.1e6d76242d560706f7df6156aa518191.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2be1d38b22977af8b234a085a8085f33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c29a0e50af4729e5f29",
+    "url": "/static/css/main.77510b03.chunk.css"
+  },
+  {
+    "revision": "0cd2ea6b18441eb3b0e5",
+    "url": "/static/js/2.cec381b7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.cec381b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c29a0e50af4729e5f29",
+    "url": "/static/js/main.69ab0fec.chunk.js"
+  },
+  {
+    "revision": "5db6dfe7a8e2b4135dd2",
+    "url": "/static/js/runtime-main.aa739829.js"
+  }
+]);
